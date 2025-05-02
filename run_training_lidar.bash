45c5ba787388c1576daf294f9c789183548a5290
tanguyguyot <guyot.tanguy1@gmail.com>
2025-05-02 13:53:32 +0200

    fix: bash file

--- run_training_lidar.bash
+++ run_training_lidar.bash
@@ -1,6 +1,6 @@
 # Run Lidar training
 
-echo "Running YOLOv8 training for LiDAR data... Getting requirements"
+echo "Running YOLOv5m training for LiDAR data... Getting requirements"
 pip install -r requirements.txt
 cd lidar_viewer
 
@@ -10,17 +10,10 @@ cd yolo_poles_lidar
 
 echo "Launching training for LiDAR..."
 rm -r runs/detect/train
-yolo detect train data=data.yaml model=yolov8m.pt epochs=50 imgsz=640
-
-echo "Training complete. Copying results to Downloads folder..."
-# mkdir -p ~/Downloads/yolo_results_lidar_train
-# scp -r runs/detect/train/ ~/Downloads/yolo_results_lidar_train
-
+yolo detect train data=data.yaml model=yolov5m.pt epochs=50 imgsz=640
 echo "Best model saved to : runs/detect/train/weights/best.pt"
-echo "Running YOLOv8 validation..."
+
+echo "Running YOLOv5m validation..."
 rm -r runs/detect/val
 yolo detect val model=runs/detect/train/weights/best.pt data=data.yaml imgsz=640
-
-echo "Validation complete. Copying results to Downloads folder..."
-# mkdir -p ~/Downloads/yolo_results_lidar_val
-#scp -r runs/detect/val/ ~/Downloads/yolo_results_lidar_val
+echo "Validation complete, accessible through the runs/detect/val folder."
--- run_training_rgb.bash
+++ run_training_rgb.bash
@@ -1,7 +1,7 @@
 
 # RGB dataset
 
-echo "Running YOLOv5 training for RGB data... Getting requirements"
+echo "Running YOLOv8 training for RGB data... Getting requirements"
 pip install -r requirements.txt
 cd rgb_viewer
 
@@ -15,19 +15,12 @@ cd yolo_poles_rgb
 echo "Launching training for RGB..."
 rm -r runs/detect/train
 yolo detect train data=data.yaml model=yolov8n.pt epochs=50 imgsz=640
-
-echo "Training complete. Copying results to Downloads folder..."
-mkdir -p ~/Downloads/yolo_results_rgb_train
-#scp -r runs/detect/train/ ~/Downloads/yolo_results_rgb_train
 echo "Best model saved to : runs/detect/train/weights/best.pt"
 
 echo "Running YOLOv8 validation..."
 rm -r runs/detect/val
 yolo detect val model=runs/detect/train/weights/best.pt data=data.yaml imgsz=640
+echo "Validation complete, accessible through the runs/detect/val folder."
 
-echo "Validation complete. Copying results to Downloads folder..."
-mkdir -p ~/Downloads/yolo_results_rgb_val
-#scp -r runs/detect/val/ ~/Downloads/yolo_results_rgb_val
-echo "All done! Results are in ~/Downloads/yolo_results_val"
 echo "You can now run the RGB viewer with the trained model."
 
